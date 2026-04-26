40b15732e83efddd7e46d43930128170e7892b63
martinesq <martinesquivel207@gmail.com>
2026-04-25 21:18:59 -0300

    ORM implementada -Implemente Entity Framework. -StringConnection esta oculta para mayor seguridad.

---
+++ DAL/DBConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class DBConfigurations
+    {
+        public static string getDbConectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+
+        }
+    }
+}
---
+++ DAL/Models/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer(DBConfigurations.getDbConectionString());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
